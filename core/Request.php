c3abfad6f9f1c2f1439fe99b2e2691cb3c30558e
Hélio Rosado Neto <rosadohelio@hotmail.com>
2023-09-21 21:20:31 -0300

    Atualizar o Request.php
    modifiquei o "/" da classe de request, foi removido no retorno antes da URI.

--- core/Request.php
+++ core/Request.php
@@ -8,7 +8,7 @@ class Request {
     public static function getUrl() {
         $url = filter_input(INPUT_GET, 'request');
         $url = str_replace(Config::BASE_DIR, '', $url);
-        return '/'.$url;
+        return $url;
     }
 
     public static function getMethod() {
