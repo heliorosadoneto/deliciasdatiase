5279c575f2dd796dce81f5c7f658e68d511037b0
Hélio Rosado Neto <82285336+heliorosadoneto@users.noreply.github.com>
2023-10-01 16:25:42 -0300

    m

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 
     public function __construct()
     {
-        $loggedUser =  LoginHandlers::VerifyLogin();
+        $loggedUser =  '';
          if($loggedUser === false){
              $this->redirect('/login');
          }
