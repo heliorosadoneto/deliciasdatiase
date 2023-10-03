094f09ccd67072b136766867169f36cd0893942a
Hélio Rosado Neto <rosadohelio@hotmail.com>
2023-10-03 22:57:59 +0000

    novo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '39a7b60e8e872558879eba4c690fc1222b7be3d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '39a7b60e8e872558879eba4c690fc1222b7be3d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'clancats/hydrahon' => array(
+            'pretty_version' => 'v1.1.12',
+            'version' => '1.1.12.0',
+            'reference' => 'cc2fc3fd2ea082a2d531a751da49cc6a413270cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../clancats/hydrahon',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
